2d3e35a93b954793bc8e57cfaf93118bf2da463d
donpelumos <donpelumos@gmail.com>
2020-07-10 23:10:03 +0100

    setting up.

---
+++ src/main/java/com/example/oauth2jwtdemo/Oauth2JwtDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2jwtdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2JwtDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2JwtDemoApplication.class, args);
+    }
+
+}
